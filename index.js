eb7ca46e33689afe64c24dd7ddb7f356ac5e84a6
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-08-25 14:41:13 +0530

    server modifications

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/search', (req, res)=>{
+app.get('/', (req, res)=>{
     res.render('search');
 });
+
+app.get('/result', (req, res)=>{
+    res.render('result');
+})
 
 app.listen(port, ()=>{
     console.log(`Server has started on port ${port}`);
