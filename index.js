be4d286bb0dac3299d59a8582622ed102bfe717c
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-08-26 04:10:21 +0530

    port number

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const request = require('request');
 
-const port = process.env.PORT || 4444;
+const port = process.env.PORT || 8080;
 
 const app = express();
 
